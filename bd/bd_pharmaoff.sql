afa8515c7a2de01ee3f3d309095d663e74325d31
Víctor Azevedo de Oliveira <69988082+VictorAzevedoOliveira@users.noreply.github.com>
2021-05-31 23:22:08 -0300

    make it better

--- bd/bd_pharmaoff.sql
+++ bd/bd_pharmaoff.sql
@@ -3,7 +3,7 @@ use db_PharmaOff;
 
 #TABLETION
 create table tb_usuario(
-id_usuario int primary key ,
+id_usuario int primary key not null,
 nme_usuario varchar (10) ,
 email_usuario varchar (50) not null,
 pwd_usuario varchar (500) not null,
@@ -11,20 +11,20 @@ pwd_changed bigint  default null
 );
 
 Create table tb_estabelecimento (
-id_estabelecimento int primary key,  
-nme_estabelecimento varchar (100) default null,
+id_estabelecimento int primary key not null,  
+nme_estabelecimento varchar (100),
 rating_estabelecimento double precision ,
 endereço_estabelecimento varchar(50) ,
-img_estabelecimento varchar(500) default null,
+img_estabelecimento varchar(500) ,
 sts_estabelecimento boolean    
 );
 
 CREATE TABLE ta_produto (
-id_produto int primary key,
-nme_produto varchar(40) DEFAULT NULL,
+id_produto int primary key not null,
+nme_produto varchar(40) ,
 preco_produto double precision ,
 preco_desconto_produto int,
-desc_produto varchar(400) DEFAULT NULL,
+desc_produto varchar(400) ,
 img_produto varchar(500) ,
 cod_estabelecimento int REFERENCES tb_estabelecimento (id_estabelecimento)
 
@@ -33,26 +33,26 @@ cod_estabelecimento int REFERENCES tb_estabelecimento (id_estabelecimento)
 
 
 create table ta_cupom(
-id_cupom int primary key ,
+id_cupom int primary key not null,
 nme_cupom  varchar(40) ,
 percent_cupom double precision ,
 dta_validade_cupom timestamp,
 sts_ativo_cupom boolean,
 cod_produto int references ta_produto (id_produto),
-cod_estabelecimento int references tb_estabelecimento (id_estabelecimento),
+cod_estabelecimento int references tb_estabelecimento (id_estabelecimento)
 );
 
 -- Lista de desejos das usuarios, cada usuario tem sua lista. 
 create table tb_listaDesejos(
-id_listaDesejos int primary key ,
+id_listaDesejos int primary key not null ,
 cod_usuario int REFERENCES tb_usuario (id_usuario)	
 );
 
 -- Lista de desejos dos produtos, que estão linkados com as usuarios da outra lista.
 create table ta_listaDesejos_produtos(
-id_listaDesejos_produtos int primary key ,
+id_listaDesejos_produtos int primary key not null,
 cod_listadesejos int  REFERENCES tb_listaDesejos (id_listadesejos),
-cod_produto int  REFERENCES ta_produto (id_produto),    
+cod_produto int  REFERENCES ta_produto (id_produto)    
 );
 
 CREATE TABLE senhatokenreset (
--- bd/index.js
+++ bd/index.js
@@ -1,11 +1,12 @@
 const  Pool  = require("pg").Pool;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'db_pharmaoff',
-  password: 'root',
-  port: 5432
+  user: 'uyhhjyeuezfpwa',
+  host: 'ec2-23-23-128-222.compute-1.amazonaws.com',
+  database: 'd33nsk255gbf7i',
+  password: '80317fe736384942e91ab6cd20af4544c6d6eebc8a6d88b81dbeb25303c31ab4',
+  port: 5432,
+  ssl: { rejectUnauthorized: true }
 });
 
 pool
