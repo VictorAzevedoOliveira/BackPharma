96cf4ec681fb21734478d28e20194642319f4e33
Víctor Azevedo de Oliveira <69988082+VictorAzevedoOliveira@users.noreply.github.com>
2021-06-01 22:11:38 -0300

    Update bd_pharmaoff.sql

--- bd/bd_pharmaoff.sql
+++ bd/bd_pharmaoff.sql
@@ -3,7 +3,7 @@ use db_PharmaOff;
 
 #TABLETION
 create table tb_usuario(
-id_usuario int primary key not null,
+id_usuario SERIAL primary key not null,
 nme_usuario varchar (10) ,
 email_usuario varchar (50) not null,
 pwd_usuario varchar (500) not null,
@@ -11,7 +11,7 @@ pwd_changed bigint  default null
 );
 
 Create table tb_estabelecimento (
-id_estabelecimento int primary key not null,  
+id_estabelecimento SERIAL primary key not null,  
 nme_estabelecimento varchar (100),
 rating_estabelecimento double precision ,
 endereço_estabelecimento varchar(50) ,
@@ -20,7 +20,7 @@ sts_estabelecimento boolean
 );
 
 CREATE TABLE ta_produto (
-id_produto int primary key not null,
+id_produto SERIAL primary key not null,
 nme_produto varchar(40) ,
 preco_produto double precision ,
 preco_desconto_produto int,
@@ -33,7 +33,7 @@ cod_estabelecimento int REFERENCES tb_estabelecimento (id_estabelecimento)
 
 
 create table ta_cupom(
-id_cupom int primary key not null,
+id_cupom SERIAL primary key not null,
 nme_cupom  varchar(40) ,
 percent_cupom double precision ,
 dta_validade_cupom timestamp,
@@ -44,19 +44,19 @@ cod_estabelecimento int references tb_estabelecimento (id_estabelecimento)
 
 -- Lista de desejos das usuarios, cada usuario tem sua lista. 
 create table tb_listaDesejos(
-id_listaDesejos int primary key not null ,
+id_listaDesejos SERIAL primary key not null ,
 cod_usuario int REFERENCES tb_usuario (id_usuario)	
 );
 
 -- Lista de desejos dos produtos, que estão linkados com as usuarios da outra lista.
 create table ta_listaDesejos_produtos(
-id_listaDesejos_produtos int primary key not null,
+id_listaDesejos_produtos SERIAL primary key not null,
 cod_listadesejos int  REFERENCES tb_listaDesejos (id_listadesejos),
 cod_produto int  REFERENCES ta_produto (id_produto)    
 );
 
 CREATE TABLE senhatokenreset (
-  id_token INT  REFERENCES tb_usuario (id_usuario),
+  id_token SERIAL  REFERENCES tb_usuario (id_usuario),
   nme_token TEXT ,
   expira_token BIGINT 
 );
