2b0bb58f7833cef1e82f7ca84c7fbe1f1b19f176
Víctor Azevedo de Oliveira <69988082+VictorAzevedoOliveira@users.noreply.github.com>
2021-06-03 15:58:56 -0300

    teste função protect2

--- utils/email.js
+++ utils/email.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
+    secure: false,
     auth: {
       user: 'pharmaoff3@gmail.com',
       pass: 'tcc40028922',
